2f7532f7bf40e79a2a6114d5761d0980ea8e91d9
Matthew Chen <awolf@MacBook-Pro.local>
2026-02-11 02:47:31 -0800

    Stop tracking build artifacts

--- cpp/build/_deps/matplotplusplus-build/source/matplot/CMakeFiles/matplot.dir/util/popen.cpp.o.d
+++
@@ -1,4 +0,0 @@
-_deps/matplotplusplus-build/source/matplot/CMakeFiles/matplot.dir/util/popen.cpp.o: \
- /home/matthewchen132/6DOF_Missile_Pursuit_RK4/cpp/build/_deps/matplotplusplus-src/source/matplot/util/popen.cpp \
- /usr/include/stdc-predef.h \
- /home/matthewchen132/6DOF_Missile_Pursuit_RK4/cpp/build/_deps/matplotplusplus-src/source/matplot/util/popen.h
